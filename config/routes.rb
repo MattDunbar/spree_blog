97d6eb485f95bd94be4540c00eec4396c27154e0
Matt Dunbar <matt@buildrx.com>
2015-04-23 03:35:44 -0400

    disabled tags

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Spree::Core::Engine.add_routes do
   namespace :blog do
     resources :posts, only: [:index, :show]
     resources :categories, only: [:show]
-    resources :tags, only: [:show]
+    #resources :tags, only: [:show]
   end
   namespace :admin do
     namespace :blog do
       resources :posts
       resources :categories
-      resources :tags
+      #resources :tags
     end
   end
 end
