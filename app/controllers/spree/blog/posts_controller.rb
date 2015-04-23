3e2fdb18820f78653a8ddbf5f31569f66869c544
Matt Dunbar <matt@buildrx.com>
2015-04-23 03:32:54 -0400

    404 for posts in hidden categories, even if linked to directly

--- app/controllers/spree/blog/posts_controller.rb
+++ app/controllers/spree/blog/posts_controller.rb
@@ -9,6 +9,9 @@ module Spree
       def show
         @categories = Spree::Blog::Category.where(hidden: false)
         @post = Spree::Blog::Post.friendly.find(params[:id])
+        if @post.category.hidden
+          raise ActiveRecord::RecordNotFound
+        end
       end
     end
   end
