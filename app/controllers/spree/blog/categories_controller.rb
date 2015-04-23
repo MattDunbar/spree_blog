008945583774925ccab4c136e80d7227d55429a5
Matt Dunbar <matt@buildrx.com>
2015-04-23 03:26:44 -0400

    retrieve all categories when within one for display

--- app/controllers/spree/blog/categories_controller.rb
+++ app/controllers/spree/blog/categories_controller.rb
@@ -2,6 +2,7 @@ module Spree
   module Blog
     class CategoriesController < Spree::StoreController
       def show
+        @categories = Spree::Blog::Category.where(hidden: false)
         @category = Spree::Blog::Category.friendly.find(params[:id])
       end
     end
